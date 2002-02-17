d7772ee7a54c9d3ea652be966a18f88355b0cc81
Dan Langille <dan@langille.org>
2002-02-17 20:18:05 +0000

    oops, never put this into CVS
    git-svn-id: svn://127.0.0.16/freshports-1/daemontools@963 46e82423-29d8-e211-989e-002590a4cdd4

---
+++ trunk/freshports/fp-daemon.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+touch ${HOME}/laststarted
+#echo start
+MSGSDIR=${HOME}/msgs/FreeBSD/incoming
+
+cd ${MSGSDIR}
+while .
+	do
+	FILES=`echo *`
+
+	if [ "$FILES" != "*" ]
+	then
+		for i in $FILES
+			do
+			$HOME/scripts/test-freebsd-cvs.sh $i
+		done
+	fi
+#	echo .
+	sleep 1
+done
