648c351e009c97610eda060ca7dd54ad149dd291
Dan Langille <dan@langille.org>
2002-02-24 02:31:04 +0000

    move failed files to a new retry directory
    git-svn-id: svn://127.0.0.16/freshports-1/daemontools@1052 46e82423-29d8-e211-989e-002590a4cdd4

--- trunk/freshports/fp-daemon.sh
+++ trunk/freshports/fp-daemon.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 #
-# $Id: fp-daemon.sh,v 1.3 2002-02-22 17:03:37 dan Exp $
+# $Id: fp-daemon.sh,v 1.4 2002-02-24 02:31:04 dan Exp $
 #
 # Copyright (c) 2001-2002 DVL Software
 #
@@ -16,6 +16,18 @@ while .
 		for i in $FILES
 			do
 			$HOME/scripts/freebsd-cvs.sh $i
+
+			RESULT=$?
+
+			echo result=$RESULT
+
+			if [ $RESULT -eq 0 ]
+			then
+				mv $i $HOME/msgs/FreeBSD/raw/
+			else
+				echo $i fails....
+				mv $i $HOME/msgs/FreeBSD/retry/
+			fi
 		done
 	fi
 	sleep 1
