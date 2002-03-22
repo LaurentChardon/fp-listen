24bbc1e459384213fb080f390816fb5fdae27dfc
Dan Langille <dan@langille.org>
2002-03-22 15:07:43 +0000

    Tidy up directories
    git-svn-id: svn://127.0.0.16/freshports-1/daemontools@1148 46e82423-29d8-e211-989e-002590a4cdd4

--- trunk/freshports-cache/fp-refresh.sh
+++ trunk/freshports-cache/fp-refresh.sh
@@ -1,8 +1,10 @@
 #!/bin/sh
 #
-# $Id: fp-refresh.sh,v 1.2 2002-02-22 17:03:39 dan Exp $
+# $Id: fp-refresh.sh,v 1.3 2002-03-22 15:07:43 dan Exp $
 #
 # Copyright (c) 2001-2002 DVL Software
 #
 
-/usr/bin/perl ${HOME}/scripts/main-page-update.pl
+BASEDIR=${HOME}
+
+/usr/bin/perl ${BASEDIR}/scripts/main-page-update.pl
