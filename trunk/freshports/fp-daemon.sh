49f1a504987070e5c03c446b896f3fb4214544ea
Dan Langille <dan@langille.org>
2002-03-22 15:06:47 +0000

    Tidy up the directories
    git-svn-id: svn://127.0.0.16/freshports-1/daemontools@1147 46e82423-29d8-e211-989e-002590a4cdd4

--- trunk/freshports/fp-daemon.sh
+++ trunk/freshports/fp-daemon.sh
@@ -1,10 +1,11 @@
 #!/bin/sh
 #
-# $Id: fp-daemon.sh,v 1.4 2002-02-24 02:31:04 dan Exp $
+# $Id: fp-daemon.sh,v 1.5 2002-03-22 15:06:47 dan Exp $
 #
 # Copyright (c) 2001-2002 DVL Software
 #
-MSGSDIR=${HOME}/msgs/FreeBSD/incoming
+BASEDIR=${HOME}
+MSGSDIR=${BASEDIR}/msgs/FreeBSD/incoming
 
 cd ${MSGSDIR}
 while .
@@ -15,7 +16,7 @@ while .
 	then
 		for i in $FILES
 			do
-			$HOME/scripts/freebsd-cvs.sh $i
+			${BASEDIR}/scripts/freebsd-cvs.sh $i
 
 			RESULT=$?
 
@@ -23,10 +24,10 @@ while .
 
 			if [ $RESULT -eq 0 ]
 			then
-				mv $i $HOME/msgs/FreeBSD/raw/
+				mv $i ${BASEDIR}/msgs/FreeBSD/raw/
 			else
 				echo $i fails....
-				mv $i $HOME/msgs/FreeBSD/retry/
+				mv $i ${BASEDIR}/msgs/FreeBSD/retry/
 			fi
 		done
 	fi
