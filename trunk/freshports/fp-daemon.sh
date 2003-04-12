a934cc6e8eb3b908ac56279bd660b7f60d39a80f
Dan Langille <dan@langille.org>
2003-04-12 11:22:57 +0000

    A new way of doing the queues
    git-svn-id: svn://127.0.0.16/freshports-1/daemontools@2136 46e82423-29d8-e211-989e-002590a4cdd4

--- trunk/freshports/fp-daemon.sh
+++ trunk/freshports/fp-daemon.sh
@@ -1,35 +1,76 @@
 #!/bin/sh
 #
-# $Id: fp-daemon.sh,v 1.5 2002-03-22 15:06:47 dan Exp $
+# $Id: fp-daemon.sh,v 1.6 2003-04-12 11:22:57 dan Exp $
 #
-# Copyright (c) 2001-2002 DVL Software
+# Copyright (c) 2001-2003 DVL Software
 #
-BASEDIR=${HOME}
-MSGSDIR=${BASEDIR}/msgs/FreeBSD/incoming
+#
+# include our local parameters
+#
+. config.sh
 
-cd ${MSGSDIR}
+#
+# sanity checking upon startup
+#
+
+for q in $QUEUES
+	do
+
+	if [ ! -d ${SCRIPTDIR}/${q}/scripts ]
+	then
+		echo "Required directory does not exist: ${SCRIPTDIR}/${q}/scripts/"
+		exit
+	fi
+
+	if [ ! -d ${BASEDIR}/${q}/msgs/FreeBSD/incoming ]
+	then
+		echo "Required directory does not exist: ${BASEDIR}/${q}/msgs/FreeBSD/incoming"
+		exit
+	fi
+
+	if [ ! -d ${BASEDIR}/${q}/msgs/FreeBSD/raw/ ]
+	then
+		echo "Required directory does not exist: ${BASEDIR}/${q}/msgs/FreeBSD/raw/"
+		exit
+	fi
+
+	if [ ! -d ${BASEDIR}/${q}/msgs/FreeBSD/retry/ ]
+	then
+		echo "Required directory does not exist: ${BASEDIR}/${q}/msgs/FreeBSD/retry/"
+		exit
+	fi
+done
+
 while .
 	do
-	FILES=`echo *`
+	for q in $QUEUES
+		do
+		cd ${SCRIPTDIR}/${q}/scripts/
 
-	if [ "$FILES" != "*" ]
-	then
-		for i in $FILES
-			do
-			${BASEDIR}/scripts/freebsd-cvs.sh $i
+		INCOMING=${BASEDIR}/${q}/msgs/FreeBSD/incoming
+		echo "looking in ${INCOMING}"
+		FILES=`echo ${INCOMING}/*`
 
-			RESULT=$?
+		if [ "$FILES" != "${INCOMING}/*" ]
+		then
+			for i in $FILES
+				do
+				echo "processing $i"
+				./freebsd-cvs.sh $i
 
-			echo result=$RESULT
-
-			if [ $RESULT -eq 0 ]
-			then
-				mv $i ${BASEDIR}/msgs/FreeBSD/raw/
-			else
-				echo $i fails....
-				mv $i ${BASEDIR}/msgs/FreeBSD/retry/
-			fi
-		done
-	fi
-	sleep 1
+				RESULT=$?
+				echo result=$RESULT
+				if [ $RESULT -eq 0 ]
+				then
+					mv $i ${BASEDIR}/${q}/msgs/FreeBSD/raw/
+				else
+					echo $i fails....
+					mv $i ${BASEDIR}/${q}/msgs/FreeBSD/retry/
+				fi
+			done
+		else
+			echo "nothing found there"
+		fi
+		sleep 3
+	done
 done
