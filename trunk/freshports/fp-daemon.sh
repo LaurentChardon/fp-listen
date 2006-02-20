32cbedab4b9e0c0f8345a92137d5ec3e6a719ccf
Dan Langille <dan@langille.org>
2006-02-20 17:41:40 +0000

    When OFFLINE, do not process the queue.  Otherwise, you wind up with unprocessed files in the RECENT directory.
    git-svn-id: svn://127.0.0.16/freshports-1/daemontools@3495 46e82423-29d8-e211-989e-002590a4cdd4

--- trunk/freshports/fp-daemon.sh
+++ trunk/freshports/fp-daemon.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 #
-# $Id: fp-daemon.sh,v 1.11 2005-08-03 12:19:14 dan Exp $
+# $Id: fp-daemon.sh,v 1.12 2006-02-20 17:41:40 dan Exp $
 #
 # Copyright (c) 2001-2003 DVL Software
 #
@@ -63,39 +63,44 @@ while .
 		do
 		cd ${SCRIPTDIR}/${q}/scripts/
 
-		INCOMING=${BASEDIR}/${q}/msgs/FreeBSD/incoming
-		FILES=`echo ${INCOMING}/*`
-
-		if [ "$FILES" != "${INCOMING}/*" ]
+		if [ -e 'OFFLINE' ]
 		then
-			logger fp-daemon "found stuff in ${INCOMING}"
-			for i in $FILES
-				do
-				logger fp-daemon "processing $i"
-
-				./freebsd-cvs.sh $i
-
-				RESULT=$?
-				logger fp-daemon "result=$RESULT"
-				basename=`basename ${i}`
-				if [ $RESULT -eq 0 ]
-				then
-					mv ${i} ${BASEDIR}/${q}/msgs/FreeBSD/recent/${basename}.raw
-				else
-					logger fp-daemon "$i fails...."
-
-					# move the original email to the retry directory
-					mv $i ${BASEDIR}/${q}/msgs/FreeBSD/retry/
-
-					# and any other files as well
-					mv  ${BASEDIR}/${q}/msgs/FreeBSD/recent/${basename}.* ${BASEDIR}/${q}/msgs/FreeBSD/retry/
-				fi
-
-				check_for_jobs
-			done
+			logger fp-daemon "system is OFFLINE: ${SCRIPTDIR}/${q}/scripts/OFFLINE exists"
 		else
-			check_for_jobs
-			logger fp-daemon "nothing found ${INCOMING}"
+			INCOMING=${BASEDIR}/${q}/msgs/FreeBSD/incoming
+			FILES=`echo ${INCOMING}/*`
+
+			if [ "$FILES" != "${INCOMING}/*" ]
+			then
+				logger fp-daemon "found stuff in ${INCOMING}"
+				for i in $FILES
+					do
+					logger fp-daemon "processing $i"
+
+					./freebsd-cvs.sh $i
+
+					RESULT=$?
+					logger fp-daemon "result=$RESULT"
+					basename=`basename ${i}`
+					if [ $RESULT -eq 0 ]
+					then
+						mv ${i} ${BASEDIR}/${q}/msgs/FreeBSD/recent/${basename}.raw
+					else
+						logger fp-daemon "$i fails...."
+
+						# move the original email to the retry directory
+						mv $i ${BASEDIR}/${q}/msgs/FreeBSD/retry/
+
+						# and any other files as well
+						mv  ${BASEDIR}/${q}/msgs/FreeBSD/recent/${basename}.* ${BASEDIR}/${q}/msgs/FreeBSD/retry/
+					fi
+
+					check_for_jobs
+				done
+			else
+				check_for_jobs
+				logger fp-daemon "nothing found ${INCOMING}"
+			fi
 		fi
 		sleep 3
 	done
