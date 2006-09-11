67588cf2e26b74d6abf5bbace3b6c69145ec0c8b
Dan Langille <dan@langille.org>
2006-09-11 23:05:21 +0000

    Better checking for OFFLINE.
    git-svn-id: svn://127.0.0.16/freshports-1/daemontools@3776 46e82423-29d8-e211-989e-002590a4cdd4

--- trunk/freshports/fp-daemon.sh
+++ trunk/freshports/fp-daemon.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 #
-# $Id: fp-daemon.sh,v 1.13 2006-09-08 02:01:48 dan Exp $
+# $Id: fp-daemon.sh,v 1.14 2006-09-11 23:05:21 dan Exp $
 #
 # Copyright (c) 2001-2003 DVL Software
 #
@@ -30,6 +30,7 @@ check_for_jobs() {
 	fi
 }
 
+logger -t ${LOGGERTAG} "starting up!"
 
 for q in $QUEUES
 	do
@@ -59,49 +60,58 @@ for q in $QUEUES
 	fi
 done
 
+echo things
+
 while .
 	do
 	for q in $QUEUES
 		do
 		cd ${SCRIPTDIR}/${q}/scripts/
 
+		INCOMING=${BASEDIR}/${q}/msgs/FreeBSD/incoming
+		FILES=`echo ${INCOMING}/*`
+
 		if [ -e 'OFFLINE' ]
 		then
 			logger -t ${LOGGERTAG} "system is OFFLINE: ${SCRIPTDIR}/${q}/scripts/OFFLINE exists"
+			break
 		else
-			INCOMING=${BASEDIR}/${q}/msgs/FreeBSD/incoming
-			FILES=`echo ${INCOMING}/*`
-
 			if [ "$FILES" != "${INCOMING}/*" ]
 			then
 				logger -t ${LOGGERTAG} "found stuff in ${INCOMING}"
 				for i in $FILES
-					do
-					logger -t ${LOGGERTAG} "processing $i"
-
-					./freebsd-cvs.sh $i
-
-					RESULT=$?
-					logger -t ${LOGGERTAG} "result=$RESULT"
-					basename=`basename ${i}`
-					if [ $RESULT -eq 0 ]
+				do
+					if [ -e 'OFFLINE' ]
 					then
-						mv ${i} ${BASEDIR}/${q}/msgs/FreeBSD/recent/${basename}.raw
+						logger -t ${LOGGERTAG} "system is OFFLINE: ${SCRIPTDIR}/${q}/scripts/OFFLINE exists"
+						break
 					else
-						logger -t ${LOGGERTAG} "$i fails...."
+						logger -t ${LOGGERTAG} "processing $i"
 
-						# move the original email to the retry directory
-						mv $i ${BASEDIR}/${q}/msgs/FreeBSD/retry/
+						./freebsd-cvs.sh $i
 
-						# and any other files as well
-						mv  ${BASEDIR}/${q}/msgs/FreeBSD/recent/${basename}.* ${BASEDIR}/${q}/msgs/FreeBSD/retry/
+						RESULT=$?
+						logger -t ${LOGGERTAG} "result=$RESULT"
+						basename=`basename ${i}`
+						if [ $RESULT -eq 0 ]
+						then
+							mv ${i} ${BASEDIR}/${q}/msgs/FreeBSD/recent/${basename}.raw
+						else
+							logger -t ${LOGGERTAG} "$i fails...."
+
+							# move the original email to the retry directory
+							mv $i ${BASEDIR}/${q}/msgs/FreeBSD/retry/
+
+							# and any other files as well
+							mv  ${BASEDIR}/${q}/msgs/FreeBSD/recent/${basename}.* ${BASEDIR}/${q}/msgs/FreeBSD/retry/
+						fi
+
+						check_for_jobs
 					fi
-
-					check_for_jobs
 				done
 			else
 				check_for_jobs
-				logger -t ${LOGGERTAG} "nothing found ${INCOMING}"
+#				logger -t ${LOGGERTAG} "nothing found ${INCOMING}"
 			fi
 		fi
 		sleep 3
