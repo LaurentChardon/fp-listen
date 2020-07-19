07904066320c6e5284835d9e728b47fae83c83fc
Dan Langille <dan@langille.org>
2020-07-19 15:10:30 +0000

    Stop using logger, use echo.
    daemon(8) will direct that output to syslog and from there into the correct
    log file.
    
    
    
    git-svn-id: svn://127.0.0.16/freshports-1/daemontools@5411 46e82423-29d8-e211-989e-002590a4cdd4

--- branches/git/fp-freshports/fp-freshports.sh
+++ branches/git/fp-freshports/fp-freshports.sh
@@ -1,15 +1,31 @@
 #!/bin/sh
+# Copyright Dan Langille dan@langille.org
 #
-# $Id: fp-daemon.sh,v 1.17 2006-11-10 14:08:26 dan Exp $
+# This script processes incoming XML files and loads them into the database.
+# It also looks for signals and invokes job-waiting.pl - this allows
+# various tasks to be serialized instead of parallel.  Some things should
+# occur serially. You don't want two updates needing the working copy of the
+# repo in two different states.  Someone will go home crying.
 #
-# Copyright (c) 2001-2003 DVL Software
+# This script originated as fp-daemon.sh sometime around 2000.
 #
+# Back then, it was run via https://cr.yp.to/daemontools.html
+# This was several years before daemon(8) became available.
 #
+# The script was converted to run under daemon(8) in Jul 2020.
+#
+# I say converted, when really all that happened was:
+#
+# * creation of an rc.d script
+# * package modification to install in different locations
+# * logger(1) calls changed to echo(1)
+#
+# The script itself wasn't modified much in this conversion.
+#
+
 # include our local parameters
 
 . /usr/local/etc/freshports/fp-freshports.sh
-
-LOGGERTAG='fp-freshports'
 
 CP='/bin/cp'
 
@@ -32,35 +48,35 @@ check_for_jobs() {
 	FLAG="${FRESHPORTS_FLAGDIR}/job_waiting"
 	if [ -f ${FLAG} ]
 	then
-		${LOGGER} -t ${LOGGERTAG} 'yes, there is a job waiting'
+		echo 'yes, there is a job waiting'
 		${PERL} ./job-waiting.pl
 		if [ $? -eq 0 ]
 		then
-			${LOGGER} -t ${LOGGERTAG} "job-waiting.pl finishes normally"
+			echo "job-waiting.pl finishes normally"
 		else
-			${LOGGER} -t ${LOGGERTAG} "FATAL job-waiting.pl finished with an error"
+			echo "FATAL job-waiting.pl finished with an error"
 		fi
 		rm ${FLAG}
 	fi
 }
 
-${LOGGER} -t ${LOGGERTAG} "starting up!"
+echo "starting up!"
 
 if [ ! -d ${SCRIPTDIR} ]
 then
-	${LOGGER} -t ${LOGGERTAG} "Required directory does not exist: ${SCRIPTDIR}"
+	echo "Required directory does not exist: ${SCRIPTDIR}"
 	exit
 fi
 
 if [ ! -d ${FRESHPORTS_MSGDIR}/recent ]
 then
-	${LOGGER} -t ${LOGGERTAG} "Required directory does not exist: ${FRESHPORTS_MSGDIR}/recent/"
+	echo "Required directory does not exist: ${FRESHPORTS_MSGDIR}/recent/"
 	exit
 fi
 
 if [ ! -d ${FRESHPORTS_MSGDIR}/retry ]
 then
-	${LOGGER} -t ${LOGGERTAG} "Required directory does not exist: ${FRESHPORTS_MSGDIR}/retry/"
+	echo "Required directory does not exist: ${FRESHPORTS_MSGDIR}/retry/"
 	exit	
 fi
 
@@ -74,23 +90,23 @@ while :
 
 	if [ -e 'OFFLINE' ]
 	then
-		${LOGGER} -t ${LOGGERTAG} "system is OFFLINE: ${SCRIPTDIR}/OFFLINE exists"
+		echo "system is OFFLINE: ${SCRIPTDIR}/OFFLINE exists"
 		break
 	else
 		# id the list of files is not just the directory name...
 		if [ "$FILES" != "${INCOMING}/*" ]
 		then
-			${LOGGER} -t ${LOGGERTAG} "found stuff in ${INCOMING}"
+			echo "found stuff in ${INCOMING}"
 			for file in $FILES
 			do
 				if [ -e 'OFFLINE' ]
 				then
-					${LOGGER} -t ${LOGGERTAG} "system is OFFLINE: ${SCRIPTDIR}/OFFLINE exists"
+					echo "system is OFFLINE: ${SCRIPTDIR}/OFFLINE exists"
 					break
 				else
 					# file is a fully qualfied pathname, not relative.
 					# e.g. /var/db/freshports/message-queues/incoming/2020.07.02.13.35.18.000000.1b49ab9b7bb15abe91a9e0610fa676053f8fe021.xml
-					${LOGGER} -t ${LOGGERTAG} "processing ${file}"
+					echo "processing ${file}"
 
 					# e.g. 2020.07.02.13.35.18.000000.1b49ab9b7bb15abe91a9e0610fa676053f8fe021.xml
 					filename=`basename ${file}`
@@ -99,9 +115,9 @@ while :
 					# load the XML into the database
 					#
 
-					${LOGGER} -t ${LOGGERTAG} "loading that XML into the database via load_xml_into_db_git.pl"
+					echo "loading that XML into the database via load_xml_into_db_git.pl"
 
-					${LOGGER} -t ${LOGGERTAG} ${PERL} ${SCRIPTDIR}/load_xml_into_db_git.pl ${file} ${OUTPUT}/${filename}.loading ${OUTPUT}/${filename}.errors
+					echo ${PERL} ${SCRIPTDIR}/load_xml_into_db_git.pl ${file} ${OUTPUT}/${filename}.loading ${OUTPUT}/${filename}.errors
 					${PERL} ${SCRIPTDIR}/load_xml_into_db_git.pl ${file} > ${OUTPUT}/${filename}.loading 2>${OUTPUT}/${filename}.errors
 					RESULT=$?
 
@@ -116,16 +132,16 @@ while :
 						fi
 					fi
 
-					${LOGGER} -t ${LOGGERTAG} "XML loading finished"
+					echo "XML loading finished"
 
-					${LOGGER} -t ${LOGGERTAG} "result=$RESULT"
+					echo "result=$RESULT"
 					basename=`basename ${file}`
 					if [ $RESULT -eq 0 ]
 					then
 						# the output of the command is enclosed in "''" in case the ls output starts with a - and would therefore be interpreted as an argument
- 						${LOGGER} -t ${LOGGERTAG} "'`ls -l ${file}`'"
-						${LOGGER} -t ${LOGGERTAG} "'`ls -ld ${FRESHPORTS_MSGDIR}/incoming/`'"
-						${LOGGER} -t ${LOGGERTAG} "'`ls -ld ${FRESHPORTS_MSGDIR}/recent/`'"
+ 						echo "'`ls -l ${file}`'"
+						echo "'`ls -ld ${FRESHPORTS_MSGDIR}/incoming/`'"
+						echo "'`ls -ld ${FRESHPORTS_MSGDIR}/recent/`'"
 
 						# use -p to preserve mtime
 						${CP} -p ${file} ${OUTPUT}
@@ -146,7 +162,7 @@ while :
 						# 
 						${RM} ${file}
 					else
-						${LOGGER} -t ${LOGGERTAG} "${file} fails...."
+						echo "${file} fails...."
 
 						# move the original email to the retry directory
 						${MV} ${file} ${FRESHPORTS_MSGDIR}/retry/
