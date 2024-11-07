1adeca593596085eb12722c1d1ad1e667ea0bf57
Dan Langille <dan@langille.org>
2024-11-07 22:19:38 +0000

    fix release, changing , to .
    git-svn-id: svn://127.0.0.16/freshports-1/daemontools@6115 46e82423-29d8-e211-989e-002590a4cdd4

--- branches/git/fp-listen/setup.py
+++ branches/git/fp-listen/setup.py
@@ -3,7 +3,7 @@
 from distutils.core import setup
 
 setup(name='fp-listen',
-      version='2,0,1',
+      version='2.0.2',
       description='FreshPorts listening daemon',
       author='Dan Langille',
       author_email='dan@langille.org',
