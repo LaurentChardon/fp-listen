0d0321c022f7f13f994c2986fda94760e3f1f0c7
Dan Langille <dan@langille.org>
2024-11-07 22:05:42 +0000

    Update version reference to 1.0.10
    git-svn-id: svn://127.0.0.16/freshports-1/daemontools@6111 46e82423-29d8-e211-989e-002590a4cdd4

--- branches/git/fp-listen/setup.py
+++ branches/git/fp-listen/setup.py
@@ -3,7 +3,7 @@
 from distutils.core import setup
 
 setup(name='fp-listen',
-      version='1.0.2',
+      version='1.0.10',
       description='FreshPorts listening daemon',
       author='Dan Langille',
       author_email='dan@langille.org',
