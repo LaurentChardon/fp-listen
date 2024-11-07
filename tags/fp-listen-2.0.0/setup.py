e0787f68d0a35cbab355b62e94587bf2b5e08e4d
Dan Langille <dan@langille.org>
2024-11-07 22:10:24 +0000

    'remove code for fetching categories'
    git-svn-id: svn://127.0.0.16/freshports-1/daemontools@6112 46e82423-29d8-e211-989e-002590a4cdd4

---
+++ tags/fp-listen-2.0.0/setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='fp-listen',
+      version='1.0.10',
+      description='FreshPorts listening daemon',
+      author='Dan Langille',
+      author_email='dan@langille.org',
+      url='https://github.com/FreshPorts/freshports/',
+      packages=['fp-listen'],
+     )
