7932d76c6d956fe154446b206ae4a4009c245668
Dan Langille <dan@langille.org>
2024-11-07 22:20:18 +0000

    'fix version in setup.py, it was using commas'
    git-svn-id: svn://127.0.0.16/freshports-1/daemontools@6116 46e82423-29d8-e211-989e-002590a4cdd4

---
+++ tags/fp-listen-2.0.2/setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='fp-listen',
+      version='2.0.2',
+      description='FreshPorts listening daemon',
+      author='Dan Langille',
+      author_email='dan@langille.org',
+      url='https://github.com/FreshPorts/freshports/',
+      packages=['fp-listen'],
+     )
